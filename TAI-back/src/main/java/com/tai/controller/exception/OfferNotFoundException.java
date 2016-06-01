e05bba790d7e8b16f7195fde52141a685925e4a8
Redid <dwdsuder@gmail.com>
2016-06-01 17:57:37 +0200

    proper HttpCode throwing

--- TAI-back/src/main/java/com/tai/controller/exception/OfferNotFoundException.java
+++ TAI-back/src/main/java/com/tai/controller/exception/OfferNotFoundException.java
@@ -1,4 +1,8 @@
 package com.tai.controller.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Offer not found")
 public class OfferNotFoundException extends RuntimeException {
 }
